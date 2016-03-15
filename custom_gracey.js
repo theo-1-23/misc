3d7f7cd653d8b6ee1e1e6013e35bef7566888e4b
theo-1-23 <theodore.redada@gmail.com>
2016-03-14 21:22:26 -0400

    Create custom_gracey.js

---
+++ custom_gracey.js
@@ -0,0 +1,74 @@
+
+$( document ).ready(function() {
+
+	$('#thumb-gradus').mouseenter(function(){ 
+		$('.tags-gradus').slideToggle(500);
+	}); 
+	$('#thumb-gradus').mouseleave(function(){ 
+		$('.tags-gradus').slideToggle(500);
+	}); 
+
+	$('#thumb-nove').mouseenter(function(){ 
+		$('.tags-nove').slideToggle(500);
+	}); 
+	$('#thumb-nove').mouseleave(function(){ 
+		$('.tags-nove').slideToggle(500);
+	}); 
+
+	$('#thumb-go2gether').mouseenter(function(){ 
+		$('.tags-go2gether').slideToggle(500);
+	}); 
+	$('#thumb-go2gether').mouseleave(function(){ 
+		$('.tags-go2gether').slideToggle(500);
+	}); 
+
+	$('#thumb-vsma').mouseenter(function(){ 
+		$('.tags-vsma').slideToggle(500);
+	}); 
+	$('#thumb-vsma').mouseleave(function(){ 
+		$('.tags-vsma').slideToggle(500);
+	}); 
+
+	$('#thumb-engage').mouseenter(function(){ 
+		$('.tags-engage').slideToggle(500);
+	}); 
+	$('#thumb-engage').mouseleave(function(){ 
+		$('.tags-engage').slideToggle(500);
+	}); 
+
+	$('#thumb-fortune').mouseenter(function(){ 
+		$('.tags-fortune').slideToggle(500);
+	}); 
+	$('#thumb-fortune').mouseleave(function(){ 
+		$('.tags-fortune').slideToggle(500);
+	}); 
+
+	$('#thumb-logo').mouseenter(function(){ 
+		$('.tags-logo').slideToggle(500);
+	}); 
+	$('#thumb-logo').mouseleave(function(){ 
+		$('.tags-logo').slideToggle(500);
+	}); 	
+
+	$('#thumb-cards').mouseenter(function(){ 
+		$('.tags-cards').slideToggle(500);
+	}); 
+	$('#thumb-cards').mouseleave(function(){ 
+		$('.tags-cards').slideToggle(500);
+	}); 
+
+	$('#thumb-misc').mouseenter(function(){ 
+		$('.tags-misc').slideToggle(500);
+	}); 
+	$('#thumb-misc').mouseleave(function(){ 
+		$('.tags-misc').slideToggle(500);
+	}); 	
+
+	$('#thumb-selfie').mouseenter(function(){ 
+		$('.tags-selfie').slideToggle(500);
+	}); 
+	$('#thumb-selfie').mouseleave(function(){ 
+		$('.tags-selfie').slideToggle(500);
+	}); 	
+
+});
